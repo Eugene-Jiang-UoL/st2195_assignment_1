c6e50cea594e1aa9ae72d8b9f23dd0f3ac3ecece
Eugene Jiang <eugenejiang231@gmail.com>
2024-11-19 22:11:39 -0700

    committing to clone of Assignment repo

---
+++ PA1-Rstudio.R
@@ -0,0 +1,9 @@
+a <- "1 2 3 4 5 6 7 8 9 10"
+print(a)
+
+#alternative way for funzies I guess
+
+b <- 1
+cat(b, b+1, b+2, b+3, b+4, b+5, b+6, b+7, b+8, b+9)
+           
+#does cat() count as printing for this? Not sure lol
